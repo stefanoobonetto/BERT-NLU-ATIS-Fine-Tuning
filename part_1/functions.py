2cef6b94f9031dde6ca0638bb29279e07f720a6f
bonnie <stefanoobonetto@gmail.com>
2024-05-13 16:04:37 +0200

    ordered files

---
+++ part_1/functions.py
@@ -0,0 +1,133 @@
+# Global variables
+import os
+import torch
+import json
+from pprint import pprint
+from torch.utils.data import DataLoader
+from collections import Counter
+import torch.utils.data as data
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1" # Used to report errors on CUDA side
+PAD_TOKEN = 0
+
+def load_data(path):
+    '''
+        input: path/to/data
+        output: json 
+    '''
+    dataset = []
+    with open(path) as f:
+        dataset = json.loads(f.read())
+    return dataset
+
+class Lang():
+    def __init__(self, words, intents, slots, cutoff=0):
+        self.word2id = self.w2id(words, cutoff=cutoff, unk=True)
+        self.slot2id = self.lab2id(slots)
+        self.intent2id = self.lab2id(intents, pad=False)
+        self.id2word = {v:k for k, v in self.word2id.items()}
+        self.id2slot = {v:k for k, v in self.slot2id.items()}
+        self.id2intent = {v:k for k, v in self.intent2id.items()}
+        
+    def w2id(self, elements, cutoff=None, unk=True):
+        vocab = {'pad': PAD_TOKEN}
+        if unk:
+            vocab['unk'] = len(vocab)
+        count = Counter(elements)
+        for k, v in count.items():
+            if v > cutoff:
+                vocab[k] = len(vocab)
+        return vocab
+    
+    def lab2id(self, elements, pad=True):
+        vocab = {}
+        if pad:
+            vocab['pad'] = PAD_TOKEN
+        for elem in elements:
+                vocab[elem] = len(vocab)
+        return vocab
+
+class IntentsAndSlots (data.Dataset):
+    # Mandatory methods are __init__, __len__ and __getitem__
+    def __init__(self, dataset, lang, unk='unk'):
+        self.utterances = []
+        self.intents = []
+        self.slots = []
+        self.unk = unk
+        
+        for x in dataset:
+            self.utterances.append(x['utterance'])
+            self.slots.append(x['slots'])
+            self.intents.append(x['intent'])
+
+        self.utt_ids = self.mapping_seq(self.utterances, lang.word2id)
+        self.slot_ids = self.mapping_seq(self.slots, lang.slot2id)
+        self.intent_ids = self.mapping_lab(self.intents, lang.intent2id)
+
+    def __len__(self):
+        return len(self.utterances)
+
+    def __getitem__(self, idx):
+        utt = torch.Tensor(self.utt_ids[idx])
+        slots = torch.Tensor(self.slot_ids[idx])
+        intent = self.intent_ids[idx]
+        sample = {'utterance': utt, 'slots': slots, 'intent': intent}
+        return sample
+    
+    # Auxiliary methods
+    
+    def mapping_lab(self, data, mapper):
+        return [mapper[x] if x in mapper else mapper[self.unk] for x in data]
+    
+    def mapping_seq(self, data, mapper): # Map sequences to number
+        res = []
+        for seq in data:
+            tmp_seq = []
+            for x in seq.split():
+                if x in mapper:
+                    tmp_seq.append(mapper[x])
+                else:
+                    tmp_seq.append(mapper[self.unk])
+            res.append(tmp_seq)
+        return res
+    
+def collate_fn(data):
+    def merge(sequences):
+        '''
+        merge from batch * sent_len to batch * max_len 
+        '''
+        lengths = [len(seq) for seq in sequences]
+        max_len = 1 if max(lengths)==0 else max(lengths)
+        # Pad token is zero in our case
+        # So we create a matrix full of PAD_TOKEN (i.e. 0) with the shape 
+        # batch_size X maximum length of a sequence
+        padded_seqs = torch.LongTensor(len(sequences),max_len).fill_(PAD_TOKEN)
+        for i, seq in enumerate(sequences):
+            end = lengths[i]
+            padded_seqs[i, :end] = seq # We copy each sequence into the matrix
+        # print(padded_seqs)
+        padded_seqs = padded_seqs.detach()  # We remove these tensors from the computational graph
+        return padded_seqs, lengths
+    # Sort data by seq lengths
+    data.sort(key=lambda x: len(x['utterance']), reverse=True) 
+    new_item = {}
+    for key in data[0].keys():
+        new_item[key] = [d[key] for d in data]
+        
+    # We just need one length for packed pad seq, since len(utt) == len(slots)
+    src_utt, _ = merge(new_item['utterance'])
+    y_slots, y_lengths = merge(new_item["slots"])
+    intent = torch.LongTensor(new_item["intent"])
+    
+    src_utt = src_utt.to(device) # We load the Tensor on our selected device
+    y_slots = y_slots.to(device)
+    intent = intent.to(device)
+    y_lengths = torch.LongTensor(y_lengths).to(device)
+    
+    new_item["utterances"] = src_utt
+    new_item["intents"] = intent
+    new_item["y_slots"] = y_slots
+    new_item["slots_len"] = y_lengths
+    return new_item
+
---
+++ part_1/main.py
@@ -0,0 +1,168 @@
+import torch.optim as optim
+from functions import *
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from model import ModelIAS, init_weights
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+from utils import eval_loop, train_loop
+
+hid_size = 200
+emb_size = 300
+
+DROP = True
+BIDIRECTIONAL = False
+
+lr = 2.5                             # learning rate
+clip = 5                                # Clip the gradient
+
+tmp_train_raw = load_data(os.path.join('dataset','ATIS','train.json'))
+test_raw = load_data(os.path.join('dataset','ATIS','test.json'))
+print('Train samples:', len(tmp_train_raw))
+print('Test samples:', len(test_raw))
+
+pprint(tmp_train_raw[0])
+
+portion = 0.10
+
+intents = [x['intent'] for x in tmp_train_raw] # We stratify on intents
+count_y = Counter(intents)
+
+labels = []
+inputs = []
+mini_train = []
+
+for id_y, y in enumerate(intents):
+    if count_y[y] > 1: # If some intents occurs only once, we put them in training
+        inputs.append(tmp_train_raw[id_y])
+        labels.append(y)
+    else:
+        mini_train.append(tmp_train_raw[id_y])
+# Random Stratify
+X_train, X_dev, y_train, y_dev = train_test_split(inputs, labels, test_size=portion, 
+                                                    random_state=42, 
+                                                    shuffle=True,
+                                                    stratify=labels)
+X_train.extend(mini_train)
+train_raw = X_train
+dev_raw = X_dev
+
+y_test = [x['intent'] for x in test_raw]
+
+words = sum([x['utterance'].split() for x in train_raw], []) # No set() since we want to compute 
+                                                            # the cutoff
+corpus = train_raw + dev_raw + test_raw # We do not wat unk labels, 
+                                        # however this depends on the research purpose
+slots = set(sum([line['slots'].split() for line in corpus],[]))
+intents = set([line['intent'] for line in corpus])
+
+lang = Lang(words, intents, slots, cutoff=0)
+
+out_slot = len(lang.slot2id)
+out_int = len(lang.intent2id)
+vocab_len = len(lang.word2id)
+
+model = ModelIAS(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN, drop=DROP, bidirectional=BIDIRECTIONAL).to(device)
+model.apply(init_weights)
+
+optimizer = optim.Adam(model.parameters(), lr=lr)
+criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
+
+# Create our datasets
+train_dataset = IntentsAndSlots(train_raw, lang)
+dev_dataset = IntentsAndSlots(dev_raw, lang)
+test_dataset = IntentsAndSlots(test_raw, lang)
+
+# Dataloader instantiations
+train_loader = DataLoader(train_dataset, batch_size=128, collate_fn=collate_fn,  shuffle=True)
+dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
+test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
+
+n_epochs = 200
+patience = 3
+losses_train = []
+losses_dev = []
+sampled_epochs = []
+best_f1 = 0
+for x in tqdm(range(1,n_epochs)):
+    loss = train_loop(train_loader, optimizer, criterion_slots, 
+                      criterion_intents, model, clip=clip)
+    if x % 5 == 0: # We check the performance every 5 epochs
+        sampled_epochs.append(x)
+        losses_train.append(np.asarray(loss).mean())
+        results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                      criterion_intents, model, lang)
+        losses_dev.append(np.asarray(loss_dev).mean())
+        
+        f1 = results_dev['total']['f']
+        # For decreasing the patience you can also use the average between slot f1 and intent accuracy
+        if f1 > best_f1:
+            best_f1 = f1
+            # Here you should save the model
+            patience = 3
+        else:
+            patience -= 1
+        if patience <= 0: # Early stopping with patience
+            break # Not nice but it keeps the code clean
+
+results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
+                                         criterion_intents, model, lang)    
+print('Slot F1: ', results_test['total']['f'])
+print('Intent Accuracy:', intent_test['accuracy'])
+
+plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
+plt.title('Train and Dev Losses')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.plot(sampled_epochs, losses_train, label='Train loss')
+plt.plot(sampled_epochs, losses_dev, label='Dev loss')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+# This Python script is implementing a training pipeline for a model that performs intent classification 
+# and slot filling on the ATIS (Airline Travel Information Systems) dataset. Here's a step-by-step breakdown:
+# - Imports and setup: The script imports necessary libraries and sets up some parameters like hidden 
+#   layer size (hid_size), embedding size (emb_size), learning rate (lr), and gradient clipping value (clip).
+# - Data loading and preprocessing: The script loads the ATIS training and test datasets. It then stratifies 
+#   the training data based on the intent labels, ensuring that intents occurring only once are put in the training 
+#   set. The training data is then split into a training set and a development set.
+# - Language processing: The script processes the words, slots, and intents in the corpus to create a Lang object, 
+#   which is used for converting between words/intents/slots and their corresponding IDs.
+# - Model setup: The script initializes a ModelIAS model with the specified parameters and applies weight initialization. 
+#   It also sets up the optimizer and loss functions.
+# - Data loading for training: The script creates PyTorch DataLoader objects for the training, development, and test datasets.
+# - Training loop: The script trains the model for a specified number of epochs (n_epochs). Every 5 epochs, it evaluates 
+#   the model on the development set and computes the F1 score. If the F1 score does not improve after a certain number of 
+#   epochs (patience), the training is stopped early.
+# - Evaluation: After training, the script evaluates the model on the test set and prints out the F1 score for slot filling
+#   and the accuracy for intent classification.
+# - Plotting: Finally, the script plots the training and development losses over the epochs.
+
+# The ModelIAS class has the following components:
+# - Embedding layer: This layer converts the input words (represented as integers) into dense vectors of fixed size.
+# - LSTM layer (utt_encoder): This layer encodes the input sequence into a fixed-length vector. It's a unidirectional LSTM 
+#   with a specified number of layers.
+# - Linear layers (slot_out and intent_out): These layers map the LSTM output to the slot and intent logits, respectively.
+# - Dropout layer: This layer randomly sets a fraction of input units to 0 at each update during training time, which helps 
+#   prevent overfitting. However, it's not currently used in the forward method.
+
+# The forward method of ModelIAS processes the input utterance and sequence lengths as follows:
+# - The utterance is passed through the embedding layer to get the word embeddings.
+# - The embeddings are packed into a PackedSequence object using pack_padded_sequence to avoid unnecessary computations 
+#   on padding tokens.
+# - The packed sequence is passed through the LSTM layer to get the encoded sequence and the last hidden state.
+# - The encoded sequence is unpacked using pad_packed_sequence.
+# - The unpacked sequence and the last hidden state are passed through the slot and intent output layers, respectively, to 
+#   get the slot and intent logits.
+# - The slot logits are permuted for loss computation.
+
+# The init_weights function initializes the weights of the LSTM and Linear layers in a specific way: LSTM weights are initialized 
+# with Xavier uniform or orthogonal initialization, and LSTM biases are set to 0. Linear weights are initialized with uniform 
+# distribution between -0.01 and 0.01, and biases are set to 0.01.
